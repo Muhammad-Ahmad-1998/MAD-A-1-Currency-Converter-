4538ac4cdecb77bbe08cc19e870d3c6e3afab3e4
Muhammad Ahmad <m.ahmad8436780@gmail.com>
2022-04-27 23:14:42 +0500

    Commit

---
+++ app/src/main/java/com/example/firstapplication/MainActivity.java
@@ -0,0 +1,64 @@
+package com.example.firstapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Adapter;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+import com.example.firstapplication.Retrofit.RetrofitBuilder;
+import com.example.firstapplication.Retrofit.RetrofitInterface;
+import com.google.gson.JsonObject;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+public class MainActivity extends AppCompatActivity {
+    Button button;
+    EditText currencyToBeConverted;
+    EditText currencyConverted;
+    TextView convertToDropdown;
+    TextView convertFromDropdown;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //Initialization
+        currencyConverted = (EditText) findViewById(R.id.currency_converted);
+        currencyToBeConverted = (EditText) findViewById(R.id.currency_to_be_converted);
+        convertToDropdown = (TextView) findViewById(R.id.convert_to);
+        convertFromDropdown = (TextView) findViewById(R.id.convert_from);
+        button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                RetrofitInterface retrofitInterface = RetrofitBuilder.getRetrofitInstance().create(RetrofitInterface.class);
+                Call<JsonObject> call = retrofitInterface.getExchangeCurrency(convertFromDropdown.getText().toString());
+                call.enqueue(new Callback<JsonObject>() {
+                    @Override
+                    public void onResponse(Call<JsonObject> call, Response<JsonObject> response) {
+                        JsonObject res = response.body();
+                        JsonObject rates = res.getAsJsonObject("rates");
+                        double currency = Double.valueOf(currencyToBeConverted.getText().toString());
+                        double multiplier = Double.valueOf(rates.get(convertToDropdown.getText().toString()).toString());
+                        double result = currency * multiplier;
+                        currencyConverted.setText(String.valueOf(result));
+                    }
+
+                    @Override
+                    public void onFailure(Call<JsonObject> call, Throwable t) {
+                    }
+                });
+            }
+        });
+    }
+}
